7e2fcd1a871405a44947dcec1bdd4333e1bf4d0d
PrettyCoffee <jonathan-holz@outlook.com>
2020-06-28 21:51:28 +0200

    initial commit

---
+++ precache-manifest.c6d1928df4c883702447a17a47663196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d71abbd9642dd9d3a709043560b40da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56f74a1a02e9df136f6e",
+    "url": "/static/css/main.b0c46b2c.chunk.css"
+  },
+  {
+    "revision": "e63bc49ee2598b40856d",
+    "url": "/static/js/2.7bf35544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7bf35544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f74a1a02e9df136f6e",
+    "url": "/static/js/main.025c3c1a.chunk.js"
+  },
+  {
+    "revision": "5fa68aa17b25b62f5a61",
+    "url": "/static/js/runtime-main.d7aa548a.js"
+  }
+]);
